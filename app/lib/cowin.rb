5e6222afeec3ac0aece53822d2baeb7d65a1eccd
Raghu Boosetty <raghu.boosetty@copart.com>
2021-06-15 23:04:45 +0530

    user agent

--- app/lib/cowin.rb
+++ app/lib/cowin.rb
@@ -99,7 +99,12 @@ class Cowin
 
 private
   def api_get(url)
-    headers = { 'Content-Type': 'application/json' }
+    headers = {
+      'Accept-Language': 'hi_IN',
+      'Host': 'cdn-api.co-vin.in',
+      'Content-Type': 'application/json', 
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+    }
     begin
       JSON.parse(RestClient.get(url, headers))
     rescue => e
